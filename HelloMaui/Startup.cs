3068fb3b6f4c5cb9e20cdc723eef59ba7c2a53f7
YongAn <414675056@qq.com>
2021-06-20 20:04:16 +0800

    初始化发布

---
+++ HelloMaui/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+
+namespace HelloMaui
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("Brand-IconFont.ttf", "BrandIcoFont");
+                });
+        }
+    }
+}
